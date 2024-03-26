80710214fa8452e7f8f2161ac55feafe517d6c09
Phillip Davis <phdavis1027@gmail.com>
2024-03-26 14:45:22 -0400

    wip: select for messages in http_listener loop
    we use a biased select to ensure that any
    actors the listener depends on are alive before
    we continue sending them messages

--- src/http_listener.rs
+++ src/http_listener.rs
@@ -1,4 +1,4 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, net::SocketAddr};
 
 use anyhow::anyhow;
 use http_body_util::{combinators::UnsyncBoxBody, BodyExt, Empty, Full};
@@ -11,7 +11,7 @@ use hyper_util::rt::TokioIo;
 use tokio::sync::{mpsc, oneshot};
 
 use crate::{
-    error::compat::CompatibilityHyperError,
+    error::{compat::CompatibilityHyperError, ApoxyError},
     forwarder::{ForwarderHandle, ForwarderMsg, IncomingRequest, OutgoingResponse},
     local_executor::LocalExecutor,
 };
@@ -39,11 +39,14 @@ impl HttpDoorman {
     }
 }
 
-enum HttpDoormanMsg {}
+enum HttpDoormanMsg {
+    ForwarderFellDown,
+}
 
 async fn serve_connection(
     req: IncomingRequest,
     forwarder: ForwarderHandle,
+    doorman_sender: mpsc::Sender<HttpDoormanMsg>,
 ) -> Result<OutgoingResponse, CompatibilityHyperError> {
     let (tx, rx) = oneshot::channel();
     forwarder
@@ -53,29 +56,85 @@ async fn serve_connection(
 
     match rx.await {
         Ok(response) => Ok(response), // This cringe is needed to convince the compiler that the error type is correct
-        Err(e) => {
+        Err(_) => {
             let response = Response::builder()
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(Empty::new().boxed_unsync());
+                .body(
+                    Empty::<Bytes>::new()
+                        .map_err(CompatibilityHyperError::from)
+                        .boxed_unsync(),
+                )
+                .unwrap(); // UNWRAP: This conversion is infallible
+            doorman_sender.blocking_send(HttpDoormanMsg::ForwarderFellDown);
 
             Ok(response)
         }
     }
 }
 
-async fn run_http_listener(doorman: HttpDoorman) {
+fn handle_doorman_msg(doorman: &mut HttpDoorman, msg: Option<HttpDoormanMsg>) {
+    match msg {
+        Some(HttpDoormanMsg::ForwarderFellDown) => {
+            // Our forwarder somehow died, so let's stand a new one up.
+            doorman.forwarder_handle = ForwarderHandle::new();
+        }
+
+        None => {
+            // Something has gone horribly wrong, somehow our recevier has been closed
+        }
+    }
+}
+
+async fn handle_incoming_connection(
+    doorman: &mut HttpDoorman,
+    conn_attempt: Result<(tokio::net::TcpStream, SocketAddr), std::io::Error>,
+    loopback_sender: mpsc::Sender<HttpDoormanMsg>,
+) {
+    let forwarder = doorman.forwarder_handle.clone();
+
+    match conn_attempt {
+        Ok((stream, addr)) => {
+            doorman
+                .http_connection_servicer
+                .serve_connection_with_upgrades(
+                    TokioIo::new(stream),
+                    service_fn(move |req| {
+                        serve_connection(req, forwarder.clone(), loopback_sender.clone())
+                    }),
+                )
+                .await;
+        }
+
+        Err(e) => {
+            // Log the error
+        }
+    }
+}
+
+async fn run_http_listener(
+    mut doorman: HttpDoorman,
+    loopback_sender: mpsc::Sender<HttpDoormanMsg>,
+) {
+    loop {
+        tokio::select! {
+            biased; // We want need to stand up a new forwarder if the old one dies.
+                    // otherwise, sending more messages to them would be useless.
+
+            msg = doorman.receiver.recv() => {
+                handle_doorman_msg(&mut doorman, msg);
+            }
+
+            conn_attempt = doorman.listener.accept() => {
+                handle_incoming_connection(&mut doorman, conn_attempt, loopback_sender.clone()).await;
+            }
+        };
+    }
+    /*
     while let Ok((stream, addr)) = doorman.listener.accept().await {
-        let io = TokioIo::new(stream);
         let forwarder = doorman.forwarder_handle.clone();
 
-        doorman
-            .http_connection_servicer
-            .serve_connection_with_upgrades(
-                io,
-                service_fn(move |req| serve_connection(req, forwarder.clone())),
-            )
-            .await;
     }
+    */
 }
 
 pub struct HttpDoormanHandle {
@@ -94,6 +153,7 @@ impl HttpDoormanHandle {
             http_connection_servicer,
             receiver,
             forwarder_handle,
+            sender.clone(),
         );
         tokio::spawn(run_http_listener(actor));
 
